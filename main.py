a669b0957a062fb1b0c9b872cdb15e5ef8b938f3
Maty Adamec <91247884+MatyAdamec@users.noreply.github.com>
2022-05-19 09:17:30 +0200

    Aktualizovat main.ts, main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 basic.show_icon(IconNames.HEART)
 def on_forever():
-    pass
+    basic.show_icon(IconNames.YES)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
 basic.showIcon(IconNames.Heart)
 basic.forever(function on_forever() {
-    
+    basic.showIcon(IconNames.Yes)
 })
